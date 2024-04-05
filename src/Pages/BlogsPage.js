16bb8b2b4577e013661262485f01001aeeb44c20
Ankur Patel <ankur.patel07@outlook.com>
2024-04-05 18:41:12 +0530

    Add files via upload

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./BlogsPage.css";
 // import BlogContent from "../components/BlogContent";
+
 import BlogsNav from "../components/BlogsNav";
 
 const BlogsPage = () => {
@@ -9,24 +11,36 @@ const BlogsPage = () => {
       <BlogsNav />
       {/* <BlogContent/> */}
       <div className="Blogs">
+        <Link to="/page1">
+          <div className="Blogs-card">
+            Building Engaged Communities: Secrets to Success
+          </div>
+        </Link>
+        <Link to="/page2">
         <div className="Blogs-card">
           Building Engaged Communities: Secrets to Success
         </div>
+        </Link>
+        <Link to="/page3">
         <div className="Blogs-card">
           Building Engaged Communities: Secrets to Success
         </div>
+        </Link>
+        <Link to="/page4">
         <div className="Blogs-card">
           Building Engaged Communities: Secrets to Success
         </div>
+        </Link>
+        <Link to="/page5">
         <div className="Blogs-card">
           Building Engaged Communities: Secrets to Success
         </div>
+        </Link>
+        <Link to="/page6">
         <div className="Blogs-card">
           Building Engaged Communities: Secrets to Success
         </div>
-        <div className="Blogs-card">
-          Building Engaged Communities: Secrets to Success
-        </div>
+        </Link>
       </div>
     </div>
   );
