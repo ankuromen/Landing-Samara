beee6813b95b39bd639747d5208a3e2fa86a0979
Rafnas <101353809+Rafnas71@users.noreply.github.com>
2024-04-16 00:40:52 +0530

    Changed Home Page Background Color

--- src/components/MemoriesComponent.js
+++ src/components/MemoriesComponent.js
@@ -7,20 +7,31 @@ const MemoriesComponent = () => {
   return (
     <div className="Memories-Container">
       <div className="Memories-Content">
-        <h1>Beyond Boundaries</h1>
-        <p className="Memories-Title">Make Memories.</p>
+        <h1> Crafting tales.</h1>
+        <p className="Memories-Title">Uniting trails.</p>
       </div>
       <div className="Memories-Second">
-        <img src={eventImage} alt="Event" className="Memories-Image" />
         <div className="Memories-Text">
           <p>
-            Envtiq is a video-based events and community discovery platform where event creators and community creators find their space to thrive by attracting fans and members. We help in crafting compelling experiences at events and power communities. Seamlessly tap into events or communities based on interest. Our mission is to bridge the gap between the physical and virtual while cultivating unforgettable encounters.
+            Envtiq is a video-based events and community discovery platform
+            where event creators and community creators find their space to
+            thrive by attracting fans and members. We help in crafting
+            compelling experiences at events and power communities. Seamlessly
+            tap into events or communities based on interest. Our mission is to
+            bridge the gap between the physical and virtual while cultivating
+            unforgettable encounters.
           </p>
-          <Link to="/team"> <button className="Memories-btn">
-            <span>Click!</span>
-            <span>Learn More</span>
-          </button></Link>
+          <Link to="/team">
+            {" "}
+            <button className="Memories-btn">
+              <span>Click!</span>
+              <span>Learn More</span>
+            </button>
+          </Link>
         </div>
+      </div>{" "}
+      <div className="Memories-Image">
+        <img src={eventImage} alt="Event" className="Memories-Image-img" />
       </div>
     </div>
   );
