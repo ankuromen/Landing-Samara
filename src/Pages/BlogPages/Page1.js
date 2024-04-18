35fa8171a92e3e88bd12c0528a0d9fe9f662e65d
Rafnas <101353809+Rafnas71@users.noreply.github.com>
2024-04-18 18:07:01 +0530

    minor change

--- src/Pages/BlogPages/Page1.js
+++ src/Pages/BlogPages/Page1.js
@@ -30,71 +30,59 @@ const Page1 = () => {
             events.
           </p>
           <div className="Blog-content-text-points">
-            <ul>
-              <li>
-                Now it is the time to innovate engagements. We have entered the
-                era of the Community Creator. Unlike traditional roles that rely
-                on existing platforms' frameworks, Community Creators forge
-                their own paths. They build dedicated followings from the ground
-                up and engage directly with their fans through events,
-                merchandise, courses, and more. This direct interaction model
-                not only enhances creator autonomy but also deepens fan
-                engagement, setting the stage for a new paradigm in
-                entertainment, education, and retail.
-              </li>
-              <li>
-                This sector has seen little innovation despite its potential.
-                Major players guard their territory with outdated models that
-                stifle creativity and limit interaction, putting the vibrant
-                future of live events and community engagements at risk. At a
-                time when industries should be driven forward by innovation, the
-                live event landscape remains disappointingly static. The problem
-                may not seem like a threat in day-to-day life but if engagements
-                die down, humanity will limit its capability to do what it does
-                best; push forward.
-              </li>
-            </ul>
-          </div>
-          <div className="Blog-content-text-points">
-            <ul>
-              <li>
-                That's why we created Evntiq. Our platform represents a
-                fundamental shift in how events and communities are managed and
-                enjoyed. From immersive VR experiences at your favourite sports
-                events to exclusive, invite-only seminars, Evntiq is setting a
-                new standard for what engagement can and should look like.
-              </li>
-              <li>
-                We are driven by a single mission: to empower and elevate live
-                engagements through cutting-edge technology and innovation. Our
-                platform doesn't just facilitate management—it enhances every
-                aspect of the experience, making each event not only seamless
-                but also significantly more engaging. We aim not only to
-                revolutionize daily engagement but also use AI for frictionless
-                experiences.
-              </li>
-            </ul>
-          </div>
-          <div className="Blog-content-text-points">
-            <ul>
-              <li>
-                At Evntiq, We believe that technology should simplify, not
-                complicate. Our user centric design ensures that anyone, from
-                event organizers to community leaders, can create, run, and
-                manage their events with ease. By removing technological
-                barriers and simplifying the complex, we want to make it
-                possible for new a set of innovators to take charge; Community
-                Creators and help them focus on what truly matters: building
-                blocks to be put together to take us forward.
-              </li>
-              <li>
-                As we look to the future, Evntiq continues to innovate, always
-                pushing the boundaries of what’s possible in technology. We’re
-                not just participating in the market—we’re leading it,
-                championing a future where every interaction is impactful and
-                every community engagement is transformative.
-              </li>
-            </ul>
+            <p>
+              Now it is the time to innovate engagements. We have entered the
+              era of the Community Creator. Unlike traditional roles that rely
+              on existing platforms' frameworks, Community Creators forge their
+              own paths. They build dedicated followings from the ground up and
+              engage directly with their fans through events, merchandise,
+              courses, and more. This direct interaction model not only enhances
+              creator autonomy but also deepens fan engagement, setting the
+              stage for a new paradigm in entertainment, education, and retail.
+            </p>
+            <p>
+              This sector has seen little innovation despite its potential.
+              Major players guard their territory with outdated models that
+              stifle creativity and limit interaction, putting the vibrant
+              future of live events and community engagements at risk. At a time
+              when industries should be driven forward by innovation, the live
+              event landscape remains disappointingly static. The problem may
+              not seem like a threat in day-to-day life but if engagements die
+              down, humanity will limit its capability to do what it does best;
+              push forward.
+            </p>
+            <p>
+              That's why we created Evntiq. Our platform represents a
+              fundamental shift in how events and communities are managed and
+              enjoyed. From immersive VR experiences at your favourite sports
+              events to exclusive, invite-only seminars, Evntiq is setting a new
+              standard for what engagement can and should look like.
+            </p>
+            <p>
+              We are driven by a single mission: to empower and elevate live
+              engagements through cutting-edge technology and innovation. Our
+              platform doesn't just facilitate management—it enhances every
+              aspect of the experience, making each event not only seamless but
+              also significantly more engaging. We aim not only to revolutionize
+              daily engagement but also use AI for frictionless experiences.
+            </p>
+            <p>
+              At Evntiq, We believe that technology should simplify, not
+              complicate. Our user centric design ensures that anyone, from
+              event organizers to community leaders, can create, run, and manage
+              their events with ease. By removing technological barriers and
+              simplifying the complex, we want to make it possible for new a set
+              of innovators to take charge; Community Creators and help them
+              focus on what truly matters: building blocks to be put together to
+              take us forward.
+            </p>
+            <p>
+              As we look to the future, Evntiq continues to innovate, always
+              pushing the boundaries of what’s possible in technology. We’re not
+              just participating in the market—we’re leading it, championing a
+              future where every interaction is impactful and every community
+              engagement is transformative.
+            </p>
           </div>
           <h3>
             By understanding and implementing these strategies, organizers can
