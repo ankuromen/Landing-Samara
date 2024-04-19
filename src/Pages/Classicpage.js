79f016295263a2836f4b5d90179d77b61471ec03
Ankur Patel <ankur.patel07@outlook.com>
2024-04-20 03:30:07 +0530

    Scroll Effect

--- src/Pages/Classicpage.js
+++ src/Pages/Classicpage.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import {React,useEffect} from 'react'
 import ClassicsContent from '../components/ClassicsContent';
 const Classicpage = () => {
+  useEffect(()=>{
+    window.scrollTo(0, 0);
+  },[])
   return (
     <div>
       <ClassicsContent/>
