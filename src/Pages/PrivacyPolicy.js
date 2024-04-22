432d896ab985369d5cd1205d85deed8dc972bd5b
Ankur Patel <ankur.patel07@outlook.com>
2024-04-22 23:30:41 +0530

    Home Page Video

--- src/Pages/PrivacyPolicy.js
+++ src/Pages/PrivacyPolicy.js
@@ -6,7 +6,6 @@ const PrivacyPolicy = () => {
 
             <h1>Privacy Policy</h1>
             <h1>Effective Date: 8th April 2024</h1>
-            <h2>Introduction Thank you for choosing Eventiq, part of our vibrant community. We are committed to protecting your personal information and your right to privacy. If you have questions or concerns about our practices, please contact us at [info@proptimize.co].</h2>
             <div className='Privacy-Content'>
 
                 <ol>
@@ -22,8 +21,9 @@ const PrivacyPolicy = () => {
                         </ul>
 
                     </li>
+<br/>
                     <li>
-                        <p><b>How We Use Your Information:<br /></b>We process your information for service provision, marketing, analytics, and recommendations within our product. Our use of your information includes:</p><br />
+                        <p><b>How We Use Your Information:<br /></b>We process your information for service provision, marketing, analytics, and recommendations within our product. Our use of your information includes:</p>
                         <ul>
                             <li>Sending updates about our products and services.</li>
                             <li>Responding to your inquiries and requests</li>
@@ -31,24 +31,31 @@ const PrivacyPolicy = () => {
                             <li>Providing targeted information relevant to your locality</li>
                         </ul>
                     </li>
+                    <br/>
                     <li>
                         <p> <b>Disclosure of Your Information</b> We do not share your personal information with third parties unless it is necessary to offer you our services, to comply with the law, or to protect our rights.</p>
                     </li>
+                    <br/>
                     <li>
                         <p><b>Data Security</b>We aim to protect your personal information through a system of organizational and technical security measures. Despite our efforts, please be aware that no security measure is perfect or impenetrable.</p>
                     </li>
+                    <br/>
                     <li>
                         <p><b>User Rights</b> You have the right to access, correct, or delete your personal information. You can manage your information through your account settings or by contacting us directly.</p>
                     </li>
+                    <br/>
                     <li>
                         <p><b>Children’s Privacy</b> We do not knowingly collect information from children under the age of 13.</p>
                     </li>
+                    <br/>
                     <li>
                         <p><b>Changes to This Privacy Policy</b> We may update this policy periodically. The updated version will be indicated by an updated "Revised" date, and will be effective as soon as it is accessible. We will notify you of significant changes by posting a notice on our site or by directly sending you a notification.</p>
                     </li>
                 </ol>
             </div>
+            <br/>
             <h2>Contact Us For more information about our privacy practices, questions, or comments, please email us at [info@proptimize.co].</h2>
+            <br/>
         </div>
     )
 }
--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,10 +1,15 @@
 import './HomeBanner.css'
 import React from 'react';
+import BannerVid from "../Assets/heading-vid.mp4";
 import scrollanimation from '../Assets/scroll-animation.gif'
 
 const HomeBanner = () => {
     return (
       <div className="Homebanner-container">
+        <video className="homebanner-video" autoPlay muted loop>
+          <source src={BannerVid} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
         <div className="Homebanner-text">
           <h1>Unifying passions.</h1>
           <h1>Amplifying experiences. </h1>
