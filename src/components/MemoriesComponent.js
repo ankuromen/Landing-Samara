613967b08857abfce6ab18eeee1e796c8a3ba571
Ankur Patel <ankur.patel07@outlook.com>
2024-04-22 22:13:32 +0530

    Memories Container

--- src/components/MemoriesComponent.js
+++ src/components/MemoriesComponent.js
@@ -11,25 +11,29 @@ const MemoriesComponent = () => {
         <p className="Memories-Title">Uniting trails.</p>
       </div>
       <div className="Memories-Second">
-        <div className="Memories-Text">
-          <p>
-            Envtiq is a video-based events and community discovery platform
-            where event creators and community creators find their space to
-            thrive by attracting fans and members. We help in crafting
-            compelling experiences at events and power communities. Seamlessly
-            tap into events or communities based on interest. Our mission is to
-            bridge the gap between the physical and virtual while cultivating
-            unforgettable encounters.
-          </p>
-          <Link to="/team">
-            {" "}
-            <button className="glow-on-hover" type="button">Learn More</button>
-          </Link>
-        </div>
+        <section className="Impromtu-Text">
+          <div className="Memories-Image">
+            <img src={eventImage} alt="Event" className="Memories-Image-img" />
+          </div>
+          <div className="Memories-Text">
+            <p>
+              Envtiq is a video-based events and community discovery platform
+              where event creators and community creators find their space to
+              thrive by attracting fans and members. We help in crafting
+              compelling experiences at events and power communities. Seamlessly
+              tap into events or communities based on interest. Our mission is to
+              bridge the gap between the physical and virtual while cultivating
+              unforgettable encounters.
+            </p>
+            <Link to="/team">
+              {" "}
+              <button className="glow-on-hover" type="button">Learn More</button>
+            </Link>
+          </div>
+        </section>
+
       </div>{" "}
-      <div className="Memories-Image">
-        <img src={eventImage} alt="Event" className="Memories-Image-img" />
-      </div>
+
     </div>
   );
 };
