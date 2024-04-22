2a8a5b758c4ddeff00ec78f94392c3246583130b
Ankur Patel <ankur.patel07@outlook.com>
2024-04-22 23:42:46 +0530

    Spacification in Privacy Policy

--- src/Pages/PrivacyPolicy.js
+++ src/Pages/PrivacyPolicy.js
@@ -5,7 +5,7 @@ const PrivacyPolicy = () => {
         <div className='Privacy-Container'>
 
             <h1>Privacy Policy</h1>
-            <h1>Effective Date: 8th April 2024</h1>
+            <h2>Effective Date: 8th April 2024</h2>
             <div className='Privacy-Content'>
 
                 <ol>
@@ -33,28 +33,28 @@ const PrivacyPolicy = () => {
                     </li>
                     <br/>
                     <li>
-                        <p> <b>Disclosure of Your Information</b> We do not share your personal information with third parties unless it is necessary to offer you our services, to comply with the law, or to protect our rights.</p>
+                        <p> <b>Disclosure of Your Information</b><br /> We do not share your personal information with third parties unless it is necessary to offer you our services, to comply with the law, or to protect our rights.</p>
                     </li>
                     <br/>
                     <li>
-                        <p><b>Data Security</b>We aim to protect your personal information through a system of organizational and technical security measures. Despite our efforts, please be aware that no security measure is perfect or impenetrable.</p>
+                        <p><b>Data Security</b><br />We aim to protect your personal information through a system of organizational and technical security measures. Despite our efforts, please be aware that no security measure is perfect or impenetrable.</p>
                     </li>
                     <br/>
                     <li>
-                        <p><b>User Rights</b> You have the right to access, correct, or delete your personal information. You can manage your information through your account settings or by contacting us directly.</p>
+                        <p><b>User Rights</b> <br />You have the right to access, correct, or delete your personal information. You can manage your information through your account settings or by contacting us directly.</p>
                     </li>
                     <br/>
                     <li>
-                        <p><b>Children’s Privacy</b> We do not knowingly collect information from children under the age of 13.</p>
+                        <p><b>Children’s Privacy</b><br /> We do not knowingly collect information from children under the age of 13.</p>
                     </li>
                     <br/>
                     <li>
-                        <p><b>Changes to This Privacy Policy</b> We may update this policy periodically. The updated version will be indicated by an updated "Revised" date, and will be effective as soon as it is accessible. We will notify you of significant changes by posting a notice on our site or by directly sending you a notification.</p>
+                        <p><b>Changes to This Privacy Policy</b><br /> We may update this policy periodically. The updated version will be indicated by an updated "Revised" date, and will be effective as soon as it is accessible. We will notify you of significant changes by posting a notice on our site or by directly sending you a notification.</p>
                     </li>
                 </ol>
             </div>
             <br/>
-            <h2>Contact Us For more information about our privacy practices, questions, or comments, please email us at [info@proptimize.co].</h2>
+            <h2>Contact Us For more information about our privacy practices, questions, or comments, please email us at info@proptimize.co</h2>
             <br/>
         </div>
     )
