9a0c72daa64da571801b6a6014e11b409bca4a93
Ankur Patel <ankur.patel07@outlook.com>
2024-04-24 08:01:24 +0530

    Minor Changes in form

--- src/components/DemoCTA.js
+++ src/components/DemoCTA.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import { React, useState } from 'react';
 import './DemoCTA.css';
+import SignupFormModal from "./SignupFormModal";
+
 
 const DemoCTA = () => {
-    return (
-      <div className="demo-cta-container">
-        <p className="cta-message">
-          Want to learn what we are building?
-          <br />
-          Book a call.
-        </p>
-        <button className="merged-button">Demo</button>
-      </div>
-    );
+  const [showSignup, setShowSignup] = useState(false);
+  return (
+    <div className="demo-cta-container">
+      <p className="cta-message">
+        Want to learn what we are building?
+        <br />
+        Book a call.
+      </p>
+      <button className="merged-button" onClick={() => setShowSignup(!showSignup)}>Demo</button>
+      {showSignup && <SignupFormModal onClose={() => setShowSignup(false)} />}
+    </div>
+
+  );
 };
 
 export default DemoCTA;
