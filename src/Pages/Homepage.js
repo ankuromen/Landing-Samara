06663e8d58342045e75c6818b394c05d358e307b
Rafnas <101353809+Rafnas71@users.noreply.github.com>
2024-05-02 01:32:40 +0530

    updates in homepage

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -8,14 +8,52 @@ import ClassicsFans from "../components/ClassicsFans";
 import DemoCTA from "../components/DemoCTA";
 import ScrollingBanner from "../components/ScrollingBanner";
 import HomeBanner from "../components/HomeBanner";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Mousewheel, Scrollbar } from "swiper/modules";
+import './HomePage.css';
 const Homepage = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  // useEffect(() => {
+  //   if (
+  //     (document.body.scrollTop < 200 ||
+  //       document.documentElement.scrollTop < 200) &&
+  //     window.onscroll
+  //   ) {
+  //     scrollFunction();
+  //   }
+  // }, [window.scroll]);
+
+  // function scrollFunction() {
+  //   console.log(window.scroll);
+  //   document
+  //     .querySelector(".Memories-Container")
+  //     .scrollIntoView({ behavior: "smooth" });
+  // }
   return (
     <div className="Homepage-Conatiner">
-      <HomeBanner/>
-      <MemoriesComponent />
+      <Swiper
+        direction={"vertical"}
+        speed={900}
+        touchReleaseOnEdges={true}
+        mousewheel={{
+          sensitivity: 2,
+          releaseOnEdges: true,
+        }}
+        modules={[Pagination, Mousewheel, Scrollbar]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <HomeBanner />
+        </SwiperSlide>
+        <SwiperSlide>
+          <MemoriesComponent />
+        </SwiperSlide>
+      </Swiper>
+
       <CommunitiesComponent />
       <ClassicsBusiness />
       <ClassicsFans />
--- src/components/ClassicsContent.js
+++ src/components/ClassicsContent.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import SignupFormModal from "./SignupFormModal";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination, Mousewheel, Scrollbar } from "swiper/modules";
